e8c238dabf6cff56466ae5653fcde8554bdd24e2
Jonathan05-creator <72807458+Jonathan05-creator@users.noreply.github.com>
2020-10-26 13:05:34 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 light.set_pixel_color(0, light.rgb(255,0 ,0 ))
+light.set_pixel_color(1, light.rgb(0,0 ,255 ))
 
--- main.ts
+++ main.ts
@@ -1,1 +1,2 @@
 light.setPixelColor(0, light.rgb(255, 0, 0))
+light.setPixelColor(1, light.rgb(0, 0, 255))
